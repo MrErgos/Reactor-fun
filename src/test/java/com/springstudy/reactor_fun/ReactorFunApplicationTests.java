0b473c3d89fe2565eb83b0be42f38a71267774ac
George <babin.svet.v@gmail.com>
2024-10-26 17:23:17 +0300

    Having fun with creation Flux

---
+++ src/test/java/com/springstudy/reactor_fun/ReactorFunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springstudy.reactor_fun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactorFunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
